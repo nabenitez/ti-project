373ceb0f92d9c9fa646ceb6d10af42a1686a209b
nabenitez <nabenitez@uc.cl>
2021-11-05 14:04:36 -0300

    fix order

--- components/Storages.js
+++ components/Storages.js
@@ -9,12 +9,12 @@ import IconButton from '@mui/material/IconButton'
 
 const columns = [
   { field: 'id', headerName: '_id', width: 250 },
+  { field: 'totalSpace', headerName: 'Espacio total', width: 100 },
+  { field: 'usedSpace', headerName: 'Espacio utilizado', width: 100 },
   { field: 'grupo', headerName: 'Grupo', width: 100 },
   { field: 'cocina', headerName: 'Cocina', width: 100 },
   { field: 'pulmon', headerName: 'Pulmon', width: 100 },
   { field: 'despacho', headerName: 'Despacho', width: 100 },
-  { field: 'totalSpace', headerName: 'Espacio total', width: 100 },
-  { field: 'usedSpace', headerName: 'Espacio utilizado', width: 100 },
 ]
 
 const Storages = () => {
